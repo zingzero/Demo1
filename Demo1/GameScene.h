0878caab7624f810ca2d83cd9ffcea5ffda969de
zingos <zingos@qq.com>
2013-07-29 14:39:46 +0800

    no message

---
+++ Demo1/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  Demo1
+//
+//  Created by Zing on 13-5-29.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameScene : CCLayer {
+    
+}
+
+@end
---
+++ Demo1/GameScene.m
@@ -0,0 +1,14 @@
+//
+//  GameScene.m
+//  Demo1
+//
+//  Created by Zing on 13-5-29.
+//  Copyright 2013年 __MyCompanyName__. All rights reserved.
+//
+
+#import "GameScene.h"
+
+
+@implementation GameScene
+
+@end
