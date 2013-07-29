0e35d750312c19c288c6c6c0a7ff8459adfade9f
zingzero <zingzero@126.com>
2013-07-29 14:42:19 +0800

    no message

--- Demo1/GameScene.m
+++ Demo1/GameScene.m
@@ -11,4 +11,32 @@
 
 @implementation GameScene
 
++(id)scene{
+    CCScene *scene = [CCScene node];
+    CCLayer *layer = [GameScene node];
+    [scene addChild:layer];
+    return scene;
+}
+
+-(id) init
+{
+    if ((self = [super init])) {
+        CCLOG(@"%@ : %@", NSStringFromSelector(_cmd), self);
+        CGSize screenSize = [[CCDirector sharedDirector]winSize];
+        
+        CCSprite *sprite = [CCSprite spriteWithFile:@"Icon-72.png"];
+        CGFloat imageHeight = [sprite texture].contentSize.height;
+        sprite.position = CGPointMake(screenSize.width/ 2, imageHeight/ 2);
+        [self addChild:sprite];
+    }
+    return self;
+}
+
+
+-(void)dealloc
+{
+    CCLOG(@"%@ : %@", NSStringFromSelector(_cmd), self);
+    [super dealloc];
+}
+
 @end
